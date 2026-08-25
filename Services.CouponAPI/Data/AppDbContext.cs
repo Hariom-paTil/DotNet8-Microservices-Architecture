8cdfa557eb3a85955a7ac54764b9913a481800a2
hariompatil <hariompatil757@gmail.com>
2026-08-25 13:34:51 +0530

    Commit 4 Created the AppDbContext and Add Required Attribute to the Coupons property

---
+++ Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.CouponAPI.Models;
+
+namespace Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    {
+    }
+}
--- Services.CouponAPI/Models/Coupon.cs
+++ Services.CouponAPI/Models/Coupon.cs
@@ -1,11 +1,17 @@
-﻿namespace Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
 
+        [Required]
         public string CouponCode { get; set; }
-
+        
+        [Required] // used of this attribute is to make sure that the DiscountAmount is
+                   // required and cannot be null
         public double DiscountAmount { get; set; }
 
         public int MinAmount { get; set; }
