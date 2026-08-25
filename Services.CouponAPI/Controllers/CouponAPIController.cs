f76c4714339a90596bb60dc42a2d8d78d751d31c
hariompatil <hariompatil757@gmail.com>
2026-08-25 15:28:14 +0530

    Commit 8 Common Response for a CouponApiController

--- Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Services.CouponAPI.Data;
 using Services.CouponAPI.Models;
+using Services.CouponAPI.Models.DTO;
 
 namespace Services.CouponAPI.Controllers
 {
@@ -10,50 +11,68 @@ namespace Services.CouponAPI.Controllers
     public class CouponAPIController : ControllerBase
     {
         private readonly AppDbContext _dbContext;
+
+
+
+
+
+
+
+        // This ResponseDto class is used to return a response from the API.
+        // It contains three properties: Result, IsSuccess, and Message.\
+        // The Result property is used to return the result of the API call,
+        // the IsSuccess property is used to indicate whether the API call was successful or not,
+        // and the Message property is used to return any error messages that may have occurred during the API call.
+
+
+        //SimplyMeans :: Every Http request will return 
+        // CouponResponceDto object which will contain Result, IsSuccess and Message properties.
+        // Frontend will check IsSuccess property to know whether the request is successful or not.
+
+        private readonly CouponResponceDto _response;
         public CouponAPIController(AppDbContext db)
         {
             _dbContext = db;
+            _response = new CouponResponceDto();
+
         }
 
         [HttpGet]
 
-        public object Get()
+        public CouponResponceDto Get()
         {
             try
             {
                 IEnumerable<Coupon> objCoupon = _dbContext.Coupons.ToList();
-                return Ok(objCoupon);
+                _response.Result= objCoupon;
+               
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
 
-            return BadRequest("Invalid Request");
-        }
+            return _response;
+       }
 
 
         [HttpGet]
         [Route("{id:int}")]
-        public object Get(int id)
+        public CouponResponceDto Get(int id)
         {
             try
             {
-                Coupon objCoupon = _dbContext.Coupons.FirstOrDefault(c => c.CouponId == id);
-                if (objCoupon != null)
-                {
-                    return Ok(objCoupon);
-                }
-                else
-                {
-                    return NotFound($"Coupon with ID {id} not found.");
-                }
+                Coupon objCoupon = _dbContext.Coupons.First(c => c.CouponId == id);
+               _response.Result = objCoupon;
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+               _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
 
+            return _response;
         }
     }
 }
---
+++ Services.CouponAPI/Models/DTO/CouponResponceDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.CouponAPI.Models.DTO
+{
+    public class CouponResponceDto
+    {
+        public object? Result { get; set; }
+
+        public bool IsSuccess { get; set; } = true;
+
+        public string? Message { get; set; } = string.Empty;
+
+
+    }
+}
