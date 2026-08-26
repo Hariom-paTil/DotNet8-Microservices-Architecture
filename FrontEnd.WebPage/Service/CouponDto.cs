cd5a7ee40591454086e8845e9f7c8e1603d6d6ab
hariompatil <hariompatil757@gmail.com>
2026-08-26 14:12:05 +0530

    FrontEnd WebPage : Coupon Service Interface

---
+++ FrontEnd.WebPage/Service/CouponDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd.WebPage.Service
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public int MinAmount { get; set; }
+    }
+}
---
+++ FrontEnd.WebPage/Service/ICouponService.cs
@@ -0,0 +1,19 @@
+﻿using FrontEnd.WebPage.Models;
+
+namespace FrontEnd.WebPage.Service
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto> GetAllCouponsAsync();
+        Task<ResponseDto> GetCouponsAsync(string couponId);
+
+        Task<ResponseDto> GetCouponsByIdAsync(int couponId);
+
+        Task<ResponseDto> CreateCouponAsync(CouponDto couponDto);
+
+        Task<ResponseDto> UpdateCouponAsync(CouponDto couponDto);\
+
+        Task<ResponseDto> DeleteCouponAsync(int couponId);
+
+    }
+}
