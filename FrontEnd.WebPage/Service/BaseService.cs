03b187ab40d32567859a6729dbd3360b476834e3
hariompatil <hariompatil757@gmail.com>
2026-08-26 14:03:53 +0530

    FrontEnd WebPage : Base Service Implementation

---
+++ FrontEnd.WebPage/Service/BaseService.cs
@@ -0,0 +1,83 @@
+﻿using FrontEnd.WebPage.Models;
+using FrontEnd.WebPage.Service.IService;
+using System.Text;
+using Newtonsoft.Json;
+using static FrontEnd.WebPage.Utility.SD;
+using System.Net;
+
+namespace FrontEnd.WebPage.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ResponseDto> SendAsync(RequestDto requestDto)
+        {
+            HttpClient client = _httpClient.CreateClient("HttpAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+
+
+
+            message.RequestUri = new Uri(requestDto.Url);
+            if(requestDto.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage? httpResponse = null;
+
+            switch(requestDto.ApiType)
+            {
+                case ApiType.GET:
+                    message.Method = HttpMethod.Get;
+                    break;
+                case ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    break;
+            }
+
+            httpResponse = await client.SendAsync(message);
+
+            switch(httpResponse.StatusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return new ResponseDto
+                    {
+                        IsSuccess = false,
+                        Message = "Resource not found."
+                    };
+                    case HttpStatusCode.InternalServerError:
+                    return new ResponseDto
+                    {
+                        IsSuccess = false,
+                        Message = "Internal server error."
+                    };
+                    case HttpStatusCode.BadRequest:
+                    return new ResponseDto
+                    {
+                        IsSuccess = false,
+                        Message = "Bad request."
+                    };
+
+                    default:
+                    var apiContent = await httpResponse.Content.ReadAsStringAsync();
+                    var apiResponse = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                    return apiResponse;
+            }
+
+        }
+    }
+}
--- FrontEnd.WebPage/Service/IService/IBaseService.cs
+++ FrontEnd.WebPage/Service/IService/IBaseService.cs
@@ -2,6 +2,11 @@
 
 namespace FrontEnd.WebPage.Service.IService
 {
+
+    // This interface defines a contract for a base service that can send HTTP requests and receive responses.
+    /// <summary>
+    /// This Was the Common Interface for all the Services to implement the SendAsync method which will be used to send HTTP requests and receive responses.
+    /// </summary>
     public interface IBaseService
     {
         Task<ResponseDto> SendAsync(RequestDto requestDto);
