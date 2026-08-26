450915ea568ac741e0af2051f447677ea12a5265
hariompatil <hariompatil757@gmail.com>
2026-08-26 13:16:51 +0530

    FrontEnd WebPage : Create Request and Response DTO With Enum

---
+++ FrontEnd.WebPage/Models/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using static FrontEnd.WebPage.Utility.SD;
+
+namespace FrontEnd.WebPage.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+
+        public string Url { get; set; } = string.Empty;
+
+        public object? Data { get; set; } = null;
+
+        public string? AccessToken { get; set; } = string.Empty;
+    }
+}
---
+++ FrontEnd.WebPage/Utility/SD.cs
@@ -0,0 +1,27 @@
+﻿namespace FrontEnd.WebPage.Utility
+{
+    public class SD
+    {
+        /// <summary>
+        ///  enumeration for API types (GET, POST, PUT, DELETE)
+        ///  suppose you want to call an api with
+        ///  GET method then you can use this enum to specify the type of api call you want to make.
+        /// its helpful to avoid hardcoding the api type in the code and also makes the code more readable.
+        /// for example, if you want to call an api with GET method then you can use SD.ApiType.GET instead of "GET" string.
+        /// enum : a special programming term used to define a custom data type holding a fixed group of named constants
+        /// 
+        /// its works predefine words so when we write it 
+        /// help reduce mixmatch of words and also help to avoid typos in the code.
+        /// 
+        /// 
+        /// 
+        /// </summary>
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
