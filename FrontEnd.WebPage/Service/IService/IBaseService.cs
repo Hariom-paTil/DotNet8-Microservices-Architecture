ac27a096e136b85045cee09b450ea2cf1d80b8ab
hariompatil <hariompatil757@gmail.com>
2026-08-26 13:28:18 +0530

    FrontEnd WebPage : Base Service Interface

---
+++ FrontEnd.WebPage/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using FrontEnd.WebPage.Models;
+
+namespace FrontEnd.WebPage.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto> SendAsync(RequestDto requestDto);
+    }
+}
