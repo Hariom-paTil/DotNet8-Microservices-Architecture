30d27fc2ff95338802498937d76a37be4e9c09e9
hariompatil <hariompatil757@gmail.com>
2026-08-27 20:16:42 +0530

    Service AuthAPI :  Endpoints for Login and Register

---
+++ Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services.AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+           
+            return Ok("User registered successfully.");
+        }
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok("User logged in successfully.");
+        }
+    }
+}
