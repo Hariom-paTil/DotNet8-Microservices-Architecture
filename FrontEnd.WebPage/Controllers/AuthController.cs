ccdf773877e1ba5cad14cc0a4a64f3aa0d9970c7
hariompatil <hariompatil757@gmail.com>
2026-08-31 09:30:24 +0530

    FronEnd WebPage : Create Auth Controller and View

---
+++ FrontEnd.WebPage/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using FrontEnd.WebPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.WebPage.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
