4ae3c5b83301a13539aa375f916b3ee922ef5ffc
hariompatil <hariompatil757@gmail.com>
2026-09-05 19:58:36 +0530

    FrontEnd WebPage :  Validation with Login and Register

--- FrontEnd.WebPage/Models/LoginRequestDto.cs
+++ FrontEnd.WebPage/Models/LoginRequestDto.cs
@@ -1,9 +1,13 @@
-﻿namespace FrontEnd.WebPage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.WebPage.Models
 {
     public class LoginRequestDto
     {
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
     }
--- FrontEnd.WebPage/Models/RegisterationRequestDto.cs
+++ FrontEnd.WebPage/Models/RegisterationRequestDto.cs
@@ -1,16 +1,25 @@
-﻿namespace FrontEnd.WebPage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.WebPage.Models
 {
     public class RegisterationRequestDTO
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
 
+        
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
+        [Required]
         public string? Role { get; set; }
 }
 }
