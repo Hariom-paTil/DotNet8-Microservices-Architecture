33f363867f3bcf51f3624b19bdcfd9055d31aeb9
hariompatil <hariompatil757@gmail.com>
2026-09-09 21:14:46 +0530

    Service ProductAPI : ProductController and Product View  Implemented

---
+++ FrontEnd.WebPage/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using FrontEnd.WebPage.Models;
+using FrontEnd.WebPage.Service.Product_Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FrontEnd.WebPage.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+           
+            var response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess)
+            {
+               ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductDto productDto)
+        {
+
+            var response = await _productService.DeleteProductAsync(productDto.ProductId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(productDto);
+        }
+    }
+}
