fe50f472074710ecb77dcf4e80d7217d21db74b0
hariompatil <hariompatil757@gmail.com>
2026-09-13 12:29:50 +0530

    FrontEnd WebPage : Details Get Action Method

--- FrontEnd.WebPage/Controllers/HomeController.cs
+++ FrontEnd.WebPage/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using FrontEnd.WebPage.Models;
 using FrontEnd.WebPage.Service.Product_Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
@@ -33,6 +34,18 @@ namespace FrontEnd.WebPage.Controllers
             return View(list);
         }
 
+        [Authorize]
+        public async Task<IActionResult> Details(int productId)
+        {
+
+            ProductDto result = new();
+            var response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess)
+            {
+                result = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            return View(result);
+        }
         public IActionResult Privacy()
         {
             return View();
