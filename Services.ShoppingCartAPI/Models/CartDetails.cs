8337684ec805c7b6a1dc67f6eb7e9f845d716128
hariompatil <hariompatil757@gmail.com>
2026-09-15 18:49:41 +0530

    Service ShoppingCartAPI : CartHeader and CartDetails Model Create

---
+++ Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        public int CartDetailsId { get; set; }
+
+        public int CartHeaderId { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+
+        public int ProductId { get; set; }
+
+
+    }
+}
---
+++ Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,28 @@
+﻿using Services.ShoppingCartAPI.Model.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string CouponCode { get; set; }
+
+
+        [NotMapped] // used to ignore this property when mapping to the database
+        // so when we apply a migration, this property will not be created in the database
+        public double Discount { get; set; }
+
+        [NotMapped]
+        public double CartTotal { get; set; }
+
+        public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
